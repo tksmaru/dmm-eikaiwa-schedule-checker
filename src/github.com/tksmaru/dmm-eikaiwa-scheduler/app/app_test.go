fb1686a4190937e2f6cbe1ef862fa71ad909b312
tksmaru <tksmaru@gmail.com>
2016-05-29 11:14:50 +0900

    added unit test for GetNotifiableLessons

--- src/github.com/tksmaru/dmm-eikaiwa-scheduler/app/app_test.go
+++ src/github.com/tksmaru/dmm-eikaiwa-scheduler/app/app_test.go
@@ -1,2 +1,42 @@
 package app
 
+import (
+	"testing"
+	"time"
+)
+
+func TestLessons_GetNotifiableLessons_OneNotifiableLessons(t *testing.T) {
+
+	date := time.Date(2014, time.December, 31, 12, 13, 24, 0, time.UTC)
+
+	l := Lessons {
+		TeacherId: "id",
+		List: []time.Time{date},
+	}
+
+	actual := l.GetNotifiableLessons([]time.Time{})
+
+	if len(actual) != 1 {
+		t.Fatalf("Notifiable lessons should have one. actual: %v", len(actual))
+	}
+
+	if !actual[0].Equal(date) {
+		t.Fatalf("Notifiable lessons should be equal to '2014-12-31 12:13:24.000 UTC'. actual: %v", actual[0])
+	}
+}
+
+func TestLessons_GetNotifiableLessons_NoNotifiableLessons(t *testing.T) {
+
+	date := time.Date(2014, time.December, 31, 12, 13, 24, 0, time.UTC)
+
+	l := Lessons {
+		TeacherId: "id",
+		List: []time.Time{date},
+	}
+
+	actual := l.GetNotifiableLessons([]time.Time{date})
+
+	if len(actual) != 0 {
+		t.Fatalf("Notifiable lessons should have none. actual: %v", len(actual))
+	}
+}
