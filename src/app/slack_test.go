ba895d4cc1c2ea7a07f0347bca156b555d46e953
tksmaru <tksmaru@gmail.com>
2016-06-10 05:28:10 +0900

    add test case for slack.

--- src/app/slack_test.go
+++ src/app/slack_test.go
@@ -1,6 +1,8 @@
 package app
 
 import (
+	"fmt"
+	"golang.org/x/net/context"
 	"google.golang.org/appengine/aetest"
 	"reflect"
 	"testing"
@@ -105,3 +107,67 @@ Hi, you can take a lesson below!
 
 Access to <http://example.com/teacher/>
 `
+
+// mock
+func mockErrorSend(ctx context.Context, m *Message) ([]byte, error) {
+	return nil, fmt.Errorf("something went wrong.")
+}
+
+func TestSlack_Send_ShouldFail_WithSendError(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer done()
+
+	m := &Message{
+		Token:    "abcdefg",
+		Channel:  "#test",
+		AsUser:   false,
+		UserName: "test_teacher from DMM Eikaiwa",
+		IconUrl:  "http://example.com/teacher/image.png",
+		Text:     expectedText,
+	}
+
+	res, err := NewSlack(ctx, mockErrorSend).Send(m)
+	if res != nil {
+		t.Fatalf("Slack_Send should return nil when send fails. actual: %v", res)
+	}
+
+	expected := "notification send failed. context: something went wrong."
+	if err.Error() != expected {
+		t.Fatalf("Slack_Send expected %v, but %v", expected, err.Error())
+	}
+}
+
+// mock
+func mockSuccessSend(ctx context.Context, m *Message) ([]byte, error) {
+	return []byte("send success"), nil
+}
+
+func TestSlack_Send_ShouldSucceed_WithoutAnyErrors(t *testing.T) {
+	ctx, done, err := aetest.NewContext()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer done()
+
+	m := &Message{
+		Token:    "abcdefg",
+		Channel:  "#test",
+		AsUser:   false,
+		UserName: "test_teacher from DMM Eikaiwa",
+		IconUrl:  "http://example.com/teacher/image.png",
+		Text:     expectedText,
+	}
+
+	b, err := NewSlack(ctx, mockSuccessSend).Send(m)
+	if err != nil {
+		t.Fatalf("Slack_Send should succeed without any arrors. actual: %v", err.Error())
+	}
+
+	expected := "send success"
+	if string(b) != expected {
+		t.Fatalf("Slack_Send expected %v, but %v", expected, string(b))
+	}
+}
